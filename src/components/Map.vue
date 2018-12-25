3561fe9eaa82fdee2e2762111f8ee6d5a9bef3ec
otta001 <anna.clausi99@gmail.com>
2018-12-25 14:03:26 +0100

    Init

---
+++ src/components/Map.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <MapCol col="0"/>
+  </div>
+</template>
+
+<script>
+import MapCol from "./MapCol.vue";
+
+export default {
+  name: "Map",
+  components: {
+    MapCol
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/MapCol.vue
@@ -0,0 +1,215 @@
+<template>
+  <div>
+    <div class="map-row">
+      <div class="map-col">
+        <img src="@/assets/map/0/21.png">
+        <img src="@/assets/map/0/20.png">
+        <img src="@/assets/map/0/19.png">
+        <img src="@/assets/map/0/18.png">
+        <img src="@/assets/map/0/17.png">
+        <img src="@/assets/map/0/16.png">
+        <img src="@/assets/map/0/15.png">
+        <img src="@/assets/map/0/14.png">
+        <img src="@/assets/map/0/13.png">
+        <img src="@/assets/map/0/12.png">
+        <img src="@/assets/map/0/11.png">
+        <img src="@/assets/map/0/10.png">
+        <img src="@/assets/map/0/9.png">
+        <img src="@/assets/map/0/8.png">
+        <img src="@/assets/map/0/7.png">
+        <img src="@/assets/map/0/6.png">
+        <img src="@/assets/map/0/5.png">
+        <img src="@/assets/map/0/4.png">
+        <img src="@/assets/map/0/3.png">
+        <img src="@/assets/map/0/2.png">
+        <img src="@/assets/map/0/1.png">
+        <img src="@/assets/map/0/0.png">
+      </div>
+      <div class="map-col">
+        <img src="@/assets/map/1/21.png">
+        <img src="@/assets/map/1/20.png">
+        <img src="@/assets/map/1/19.png">
+        <img src="@/assets/map/1/18.png">
+        <img src="@/assets/map/1/17.png">
+        <img src="@/assets/map/1/16.png">
+        <img src="@/assets/map/1/15.png">
+        <img src="@/assets/map/1/14.png">
+        <img src="@/assets/map/1/13.png">
+        <img src="@/assets/map/1/12.png">
+        <img src="@/assets/map/1/11.png">
+        <img src="@/assets/map/1/10.png">
+        <img src="@/assets/map/1/9.png">
+        <img src="@/assets/map/1/8.png">
+        <img src="@/assets/map/1/7.png">
+        <img src="@/assets/map/1/6.png">
+        <img src="@/assets/map/1/5.png">
+        <img src="@/assets/map/1/4.png">
+        <img src="@/assets/map/1/3.png">
+        <img src="@/assets/map/1/2.png">
+        <img src="@/assets/map/1/1.png">
+        <img src="@/assets/map/1/0.png">
+      </div>
+      <div class="map-col">
+        <img src="@/assets/map/2/21.png">
+        <img src="@/assets/map/2/20.png">
+        <img src="@/assets/map/2/19.png">
+        <img src="@/assets/map/2/18.png">
+        <img src="@/assets/map/2/17.png">
+        <img src="@/assets/map/2/16.png">
+        <img src="@/assets/map/2/15.png">
+        <img src="@/assets/map/2/14.png">
+        <img src="@/assets/map/2/13.png">
+        <img src="@/assets/map/2/12.png">
+        <img src="@/assets/map/2/11.png">
+        <img src="@/assets/map/2/10.png">
+        <img src="@/assets/map/2/9.png">
+        <img src="@/assets/map/2/8.png">
+        <img src="@/assets/map/2/7.png">
+        <img src="@/assets/map/2/6.png">
+        <img src="@/assets/map/2/5.png">
+        <img src="@/assets/map/2/4.png">
+        <img src="@/assets/map/2/3.png">
+        <img src="@/assets/map/2/2.png">
+        <img src="@/assets/map/2/1.png">
+        <img src="@/assets/map/2/0.png">
+      </div>
+      <div class="map-col">
+        <img src="@/assets/map/3/21.png">
+        <img src="@/assets/map/3/20.png">
+        <img src="@/assets/map/3/19.png">
+        <img src="@/assets/map/3/18.png">
+        <img src="@/assets/map/3/17.png">
+        <img src="@/assets/map/3/16.png">
+        <img src="@/assets/map/3/15.png">
+        <img src="@/assets/map/3/14.png">
+        <img src="@/assets/map/3/13.png">
+        <img src="@/assets/map/3/12.png">
+        <img src="@/assets/map/3/11.png">
+        <img src="@/assets/map/3/10.png">
+        <img src="@/assets/map/3/9.png">
+        <img src="@/assets/map/3/8.png">
+        <img src="@/assets/map/3/7.png">
+        <img src="@/assets/map/3/6.png">
+        <img src="@/assets/map/3/5.png">
+        <img src="@/assets/map/3/4.png">
+        <img src="@/assets/map/3/3.png">
+        <img src="@/assets/map/3/2.png">
+        <img src="@/assets/map/3/1.png">
+        <img src="@/assets/map/3/0.png">
+      </div>
+      <div class="map-col">
+        <img src="@/assets/map/4/21.png">
+        <img src="@/assets/map/4/20.png">
+        <img src="@/assets/map/4/19.png">
+        <img src="@/assets/map/4/18.png">
+        <img src="@/assets/map/4/17.png">
+        <img src="@/assets/map/4/16.png">
+        <img src="@/assets/map/4/15.png">
+        <img src="@/assets/map/4/14.png">
+        <img src="@/assets/map/4/13.png">
+        <img src="@/assets/map/4/12.png">
+        <img src="@/assets/map/4/11.png">
+        <img src="@/assets/map/4/10.png">
+        <img src="@/assets/map/4/9.png">
+        <img src="@/assets/map/4/8.png">
+        <img src="@/assets/map/4/7.png">
+        <img src="@/assets/map/4/6.png">
+        <img src="@/assets/map/4/5.png">
+        <img src="@/assets/map/4/4.png">
+        <img src="@/assets/map/4/3.png">
+        <img src="@/assets/map/4/2.png">
+        <img src="@/assets/map/4/1.png">
+        <img src="@/assets/map/4/0.png">
+      </div>
+      <div class="map-col">
+        <img src="@/assets/map/5/21.png">
+        <img src="@/assets/map/5/20.png">
+        <img src="@/assets/map/5/19.png">
+        <img src="@/assets/map/5/18.png">
+        <img src="@/assets/map/5/17.png">
+        <img src="@/assets/map/5/16.png">
+        <img src="@/assets/map/5/15.png">
+        <img src="@/assets/map/5/14.png">
+        <img src="@/assets/map/5/13.png">
+        <img src="@/assets/map/5/12.png">
+        <img src="@/assets/map/5/11.png">
+        <img src="@/assets/map/5/10.png">
+        <img src="@/assets/map/5/9.png">
+        <img src="@/assets/map/5/8.png">
+        <img src="@/assets/map/5/7.png">
+        <img src="@/assets/map/5/6.png">
+        <img src="@/assets/map/5/5.png">
+        <img src="@/assets/map/5/4.png">
+        <img src="@/assets/map/5/3.png">
+        <img src="@/assets/map/5/2.png">
+        <img src="@/assets/map/5/1.png">
+        <img src="@/assets/map/5/0.png">
+      </div>
+      <div class="map-col">
+        <img src="@/assets/map/6/21.png">
+        <img src="@/assets/map/6/20.png">
+        <img src="@/assets/map/6/19.png">
+        <img src="@/assets/map/6/18.png">
+        <img src="@/assets/map/6/17.png">
+        <img src="@/assets/map/6/16.png">
+        <img src="@/assets/map/6/15.png">
+        <img src="@/assets/map/6/14.png">
+        <img src="@/assets/map/6/13.png">
+        <img src="@/assets/map/6/12.png">
+        <img src="@/assets/map/6/11.png">
+        <img src="@/assets/map/6/10.png">
+        <img src="@/assets/map/6/9.png">
+        <img src="@/assets/map/6/8.png">
+        <img src="@/assets/map/6/7.png">
+        <img src="@/assets/map/6/6.png">
+        <img src="@/assets/map/6/5.png">
+        <img src="@/assets/map/6/4.png">
+        <img src="@/assets/map/6/3.png">
+        <img src="@/assets/map/6/2.png">
+        <img src="@/assets/map/6/1.png">
+        <img src="@/assets/map/6/0.png">
+      </div>
+      <div class="map-col">
+        <img src="@/assets/map/7/21.png">
+        <img src="@/assets/map/7/20.png">
+        <img src="@/assets/map/7/19.png">
+        <img src="@/assets/map/7/18.png">
+        <img src="@/assets/map/7/17.png">
+        <img src="@/assets/map/7/16.png">
+        <img src="@/assets/map/7/15.png">
+        <img src="@/assets/map/7/14.png">
+        <img src="@/assets/map/7/13.png">
+        <img src="@/assets/map/7/12.png">
+        <img src="@/assets/map/7/11.png">
+        <img src="@/assets/map/7/10.png">
+        <img src="@/assets/map/7/9.png">
+        <img src="@/assets/map/7/8.png">
+        <img src="@/assets/map/7/7.png">
+        <img src="@/assets/map/7/6.png">
+        <img src="@/assets/map/7/5.png">
+        <img src="@/assets/map/7/4.png">
+        <img src="@/assets/map/7/3.png">
+        <img src="@/assets/map/7/2.png">
+        <img src="@/assets/map/7/1.png">
+        <img src="@/assets/map/7/0.png">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Map"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.map-col {
+  width: 256px;
+  float: left;
+}
+.map-col img {
+  float: left;
+}
+</style>
